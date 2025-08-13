3d194bd02baf13b312c3ab96c9b08c9195a4ef4f
karim.rakia@adesso.de <karim.rakia@adesso.de>
2025-08-13 13:45:35 +0200

    sql

--- backend/src/main/resources/db/migration/V20250813__add_recipient_id_to_jamiah_cycle.sql
+++ backend/src/main/resources/db/migration/V20250813__add_recipient_id_to_jamiah_cycle.sql
@@ -2,4 +2,4 @@ ALTER TABLE jamiah_cycles
   ADD CONSTRAINT fk_jc_recipient
   FOREIGN KEY (recipient_id) REFERENCES user_profiles(id);
 
-CREATE INDEX idx_jc_recipient_id ON jamiah_cycle(recipient_id);
+CREATE INDEX idx_jc_recipient_id ON jamiah_cycles(recipient_id);
