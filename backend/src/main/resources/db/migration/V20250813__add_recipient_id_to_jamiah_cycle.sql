19ec9893317a12dc83e650b9fede73a9d227d852
karim.rakia@adesso.de <karim.rakia@adesso.de>
2025-08-13 13:37:07 +0200

    sql

--- backend/src/main/resources/db/migration/V20250813__add_recipient_id_to_jamiah_cycle.sql
+++ backend/src/main/resources/db/migration/V20250813__add_recipient_id_to_jamiah_cycle.sql
@@ -1,7 +1,4 @@
 ALTER TABLE jamiah_cycles
-  ADD COLUMN recipient_id BIGINT NULL;
-
-ALTER TABLE jamiah_cycles
   ADD CONSTRAINT fk_jc_recipient
   FOREIGN KEY (recipient_id) REFERENCES user_profiles(id);
 
